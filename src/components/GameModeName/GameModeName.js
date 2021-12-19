1462f3c47130863ee072fafd552a54a8e66eee05
Ryszard <49765395+Ryszard-S@users.noreply.github.com>
2021-12-19 23:03:33 +0100

    correct formatting

--- src/components/GameModeName/GameModeName.js
+++ src/components/GameModeName/GameModeName.js
@@ -2,30 +2,28 @@ import * as classes from './style.module.scss';
 
 export const GAME_MODE_COMPONENT_NAME = 'game-mode-title';
 
-class GameMode extends HTMLElement{
-    constructor (){
-        super();
-        this._contents = new DocumentFragment();
-        this.gameMode = GameMode.gameModeBuilder();
-        this._contents.appendChild(this.gameMode)
+class GameMode extends HTMLElement {
+  constructor() {
+    super();
+    this._contents = new DocumentFragment();
+    this.gameMode = GameMode.gameModeBuilder();
+    this._contents.appendChild(this.gameMode);
+  }
 
-    }
+  connectedCallback() {
+    this.appendChild(this._contents);
+  }
 
-    connectedCallback() {
-        this.appendChild(this._contents)
-      }
+  static gameModeBuilder() {
+    const gameMode = document.createElement('div');
+    gameMode.className = classes.gameModeTitle;
 
+    return gameMode;
+  }
 
-    static gameModeBuilder() {
-        const gameMode = document.createElement('div');
-        gameMode.className= classes.gameModeTitle;
-
-        return gameMode;
-    }
-
-    setText(text){
-        this.gameMode.textContent=`MODE: ${text}`
-    }
+  setText(text) {
+    this.gameMode.textContent = `MODE: ${text}`;
+  }
 }
 
-customElements.define(GAME_MODE_COMPONENT_NAME, GameMode)
+customElements.define(GAME_MODE_COMPONENT_NAME, GameMode);
