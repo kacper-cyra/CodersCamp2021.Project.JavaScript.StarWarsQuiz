1dece3f672b7205d0ba9d4ff3d420b8e1112458f
Piotr Misiak <misiak690@gmail.com>
2021-12-20 21:10:22 +0100

    Change name, add scss

---
+++ src/components/HallOfFameButton/HallOfFameButton.js
@@ -0,0 +1,6 @@
+const HallOfFameButton = document.createElement('button')
+const Icon = document.createElement('img')
+HallOfFameButton.textContent = "hall of fame"
+Icon.setAttribute('src', './static/assets/img/modes/icon/contacts_24px.png')
+document.body.append(HallOfFameButton)
+HallOfFameButton.append(Icon)
