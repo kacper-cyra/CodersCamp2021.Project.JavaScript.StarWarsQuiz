e5911104434459f6ac9d1d8bf580c04f93c9922e
Ryszard <49765395+Ryszard-S@users.noreply.github.com>
2021-12-21 17:55:22 +0100

    add saving ranking in local storage

---
+++ src/services/localStorage/LocalStorageScores.js
@@ -0,0 +1,17 @@
+export const LocalStorageScores = ({ gameMode }) => {
+  return {
+    saveScore({ playerName, score }) {
+      const save = { playerName, score };
+      const previousSave = JSON.parse(localStorage.getItem(gameMode));
+
+      if (!previousSave) {
+        localStorage.setItem(gameMode, JSON.stringify([save]));
+      } else {
+        localStorage.setItem(gameMode, JSON.stringify([...previousSave, save]));
+      }
+    },
+    readScore() {
+      return JSON.parse(localStorage.getItem(gameMode));
+    },
+  };
+};
