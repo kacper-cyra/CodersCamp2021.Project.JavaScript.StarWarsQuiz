7d6277829cfd4949f11f719ce031ba065c08e19c
Kacper Cyranowski <kacper.cyranowski@gmail.com>
2022-01-05 17:48:16 +0100

    remove unneed code

--- src/components/HallOfFameButton/HallOfFameButton.js
+++ src/components/HallOfFameButton/HallOfFameButton.js
@@ -1,73 +1,47 @@
-import { async } from 'regenerator-runtime';
 import * as classes from './style.module.scss';
 
-// const HallOfFameButton = document.createElement('button')
-// const Icon = document.createElement('img')
-// HallOfFameButton.textContent = "hall of fame"
-// Icon.setAttribute('src', './static/assets/img/modes/icon/contacts_24px.png')
-// document.body.append(HallOfFameButton)
-// HallOfFameButton.append(Icon)
-
-
-export const HALL_OF_FAME_BUTTON_COMPONENT_NAME = 'hall-of-fame-button-component';
+export const HALL_OF_FAME_BUTTON_COMPONENT_NAME =
+  'hall-of-fame-button-component';
 
 class Button extends HTMLElement {
-    constructor() {
-        super();
-        this.contents = new DocumentFragment();
+  constructor() {
+    super();
+    this.contents = new DocumentFragment();
 
-        this.icon = Button.iconBuilder();
-        this.hallOfFameButton = Button.hallOfFameButtonBuilder();
+    this.icon = Button.iconBuilder();
+    this.hallOfFameButton = Button.hallOfFameButtonBuilder();
 
+    this.contents.appendChild(this.hallOfFameButton);
+  }
 
-        this.contents.appendChild(this.hallOfFameButton)
-    }
+  connectedCallback() {
+    this.appendChild(this.contents);
+    this.hallOfFameButton.append(this.icon);
+  }
 
-    connectedCallback() {
-        this.appendChild(this.contents);
-        this.hallOfFameButton.append(this.icon);
-    }
+  static iconBuilder() {
+    const icon = document.createElement('img');
+    icon.setAttribute(
+      'src',
+      '../../static/assets/img/modes/icon/contacts_24px.png',
+    );
+    icon.className = classes.icon;
 
-    static iconBuilder() {
-        const icon = document.createElement('img')
-        icon.setAttribute('src', '../../static/assets/img/modes/icon/contacts_24px.png');
-        icon.className = classes.icon;
+    return icon;
+  }
 
-        return icon
-    }
+  static hallOfFameButtonBuilder() {
+    const hallOfFameButton = document.createElement('button');
+    hallOfFameButton.className = classes.hallOfFamebutton;
+    hallOfFameButton.textContent = 'caption';
 
-    static hallOfFameButtonBuilder() {
-        const hallOfFameButton = document.createElement('button');
-        hallOfFameButton.className = classes.hallOfFamebutton;
-        hallOfFameButton.textContent = "caption";
+    return hallOfFameButton;
+  }
 
-        return hallOfFameButton;
-    }
-
-    setParameters(imageSrc, caption) {
-        this.icon.setAttribute('src', imageSrc);
-        this.hallOfFameButton.innerText = caption;
-    }
+  setParameters(imageSrc, caption) {
+    this.icon.setAttribute('src', imageSrc);
+    this.hallOfFameButton.innerText = caption;
+  }
 }
 
-askQuestion(question, callback) {
-    callback.forEach((callback) => {
-        callback(question)
-    });
-    this._question = question;
-}
-
-isAnswerCorrect()
-
-async askQuestion(question, callback) {
-    this.question = await getQuizQuestion();
-    callback();
-}
-
-async answerQuestion(answer, callback) {
-    this.isAnswerCorrect = await isAnswerCorrect()
-    callback();
-}
-
-
-customElements.define(HALL_OF_FAME_BUTTON_COMPONENT_NAME, Button)
+customElements.define(HALL_OF_FAME_BUTTON_COMPONENT_NAME, Button);
